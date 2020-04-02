cc96dd2c921ddb31b0ba51c05d8d0dd9e70a4fdd
MazFahim <52876279+MazFahim@users.noreply.github.com>
2020-04-02 09:26:37 -0700

    Add files via upload
    This script is going to be our 1st page of the website.

---
+++ welcome.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet"  type="text/css" href="css/Home.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <title>Bashavara.com</title>
+
+    <script type="text/javascript">
+
+        function togglemenu(){
+
+        document.getElementById('sidebar').classList.toggle('active');
+        }
+    </script>
+
+    </head>
+    <body>
+      <form action="Login.php" method="post">
+        <div class="navbar">
+          <img src ="images/navbar.png"  width="60">
+        </div>
+        <div class="profile" >
+          <input type="image" src="images/profile.png" name="Profile"  width="60">
+        </div>
+        <center><h1>BashaBhara.Com</h1></center>
+        <div class="table-box">
+        <div class="table-row">
+        <div class="table-cell">
+
+            <p> <img src="images/location.png" width="30"/> Chittagong</p>
+        </div>
+        <div class="table-cell">
+        <p> Family</p>
+        </div>
+        </div>
+        </div>
+        <div id="sidebar" onclick="togglemenu()">
+        <div class="toggle-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+        </div>
+        <ul>
+        <!--<li>Newest</li>
+        <li>Oldest</li>-->
+        <li>Price(High-Low)</li>
+        <li>Rating(Highest)</li>
+        <li>Rating(Lowest)</li>
+        </ul>
+        </div>
+
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 8,500</h1>
+            <h4><img src="images/bedlogo.png" width="15"/>4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 22,000</h1>
+            <h4><img src="images/bedlogo.png" width="15"/>4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 17,000</h1>
+            <h4><img src="images/bedlogo.png" width="15"/> 4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 10,500</h1>
+            <h4><img src="images/bedlogo.png" width="15"/>4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 18,500</h1>
+            <h4> <img src="images/bedlogo.png" width="15"/> 4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 15,000</h1>
+            <h4><img src="images/bedlogo.png" width="15"/> 4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 20,000</h1>
+            <h4><img src="images/bedlogo.png" width="15"/> 4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+        <div class="part_1">
+            <p>Chadgaon abashik area,A-block...</p>
+            <h1><img src="images/taka.png" width="17"/> 12,000</h1>
+            <h4><img src="images/bedlogo.png" width="15"/> 4 &nbsp; <img src="images/bathtub.png" width="15"/> 5 &nbsp;&nbsp; 3000 sqft.</h4>
+            <a href="Login.php">See more</a>
+        </div>
+
+
+      </form>
+    </body>
+    </html>
