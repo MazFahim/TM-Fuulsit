8f4f7a900aecac8443be7e8d1c830ed69e6561b9
MazFahim <52876279+MazFahim@users.noreply.github.com>
2020-04-10 09:14:56 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,42 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="login.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+
+    <title> abcd </title>
+  </head>
+  <body>
+    <center>
+      <div class="Welcome">
+      <h1>WELCOME TO Tuition Media</h1>
+    </div>
+    </center>
+    <div>
+      <?php
+        if (isset($_SESSION['userid'])) {
+            echo '<form action="includes/logout.inc.php" method="post">
+        
+            <button type="submit" name="logout-submit">Logout</button>
+        
+            </form>';
+          }
+          else{
+            echo '<form action="includes/login.inc.php" method="post">
+            <input type="text" name="mailuid" placeholder="Username/Email"></br>
+            <input type="password" name="pwd" placeholder="password"></br>
+            <button type="submit" name="login-submit">Login</button>
+        
+      </form>
+      <a href="signup.php">Signup</a>';
+          }
+      ?>
+      
+    </div>
+  </body>
+</html>
